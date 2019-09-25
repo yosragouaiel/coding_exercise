ece50228cef34062c34651edee396ca87ca5e2e1
yosragouaiel <yosra.gouaiel@ensi-uma.tn>
2019-09-25 14:21:47 +0100

    Update parallel_monte_carlo_pi.cpp

--- parallel_monte_carlo_pi.cpp
+++ parallel_monte_carlo_pi.cpp
@@ -1,44 +1,23 @@
-// https://HelloACM.com
-
 #include <iostream>
-#include <time.h> 
-#include <ppl.h>
-#include <iomanip>
-
+#include <time.h> // time
+#include <stdlib.h> // srand
+ 
 using namespace std;
-
-int monte_carlo_count_pi(int n)
-{
-	int c = 0;
-	for (int i = 0; i < n; i ++)
-	{
-		double x = (double)rand() / (RAND_MAX);
-		double y = (double)rand() / (RAND_MAX);
-		if (x * x + y * y <= 1.0)
-		{
-			c++;
-		}
-	}
-	return c;
-}
-
-int main()
-{
-	srand(time(NULL));
-	const int N1 = 1000;
-	const int N2 = 100000;
-	int n = 0;
-	int c = 0;
-	Concurrency::critical_section cs;
-	// it is better that N1 >> N2 for better performance
-	Concurrency::parallel_for(0, N1, [&](int i)
-	{
-		int t = monte_carlo_count_pi(N2);
-		cs.lock(); // race condition
-		n += N2;   // total sampling points
-		c += t;    // points fall in the circle
-		cs.unlock();
-	});
-	cout << "pi ~= " << setprecision(9) << (double)c / n * 4.0 << endl;
-	return 0;
+ 
+int main() {
+    srand(time(NULL));
+    cout.precision(10);
+    cout << "HelloACM.com rocks!" << endl;
+    const int N[] = {1e3, 1e4, 1e5, 1e6, 1e7, 1e8, 1e9};
+    for (int j = 0; j < (sizeof(N) / sizeof(N[0])); j ++) {
+        int circle = 0;
+        for (int i = 0; i < N[j]; i ++) {
+            double x = static_cast<double>(rand()) / static_cast<double>(RAND_MAX);
+            double y = static_cast<double>(rand()) / static_cast<double>(RAND_MAX);
+            if (x * x + y * y <= 1.0) circle ++;
+        }
+ 
+        cout << N[j] << (char)9 << (char)9 << (double)circle / N[j] * 4.0 << endl;
+    }
+    return 0;
 }
